015ece15fa1f100fc4442c5cbb79d0765e0d798a
Manpreet <manpreet2197@gmail.com>
2022-06-08 00:24:21 -0400

    Changed city name to test api

--- ApiCall.py
+++ ApiCall.py
@@ -1,8 +1,9 @@
 import pip._vendor.requests
 from pip._vendor import requests
 
-city = 'Brampton'
-api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+countryCity = 'Brampton'
+api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(countryCity = 'Brampton'
+)
 response = requests.get(api_url, headers={'X-Api-Key': '85PFMA5rymDhOSCMI6zKnw==wNtRNdqXJh5UQbNG'})
 if response.status_code == requests.codes.ok:
     print(response.text)

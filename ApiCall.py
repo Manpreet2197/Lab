7415d162e0c18404a8acf283acfd4d0e119a5175
Manpreet <manpreet2197@gmail.com>
2022-06-08 00:45:57 -0400

    Update ApiCall.py

--- ApiCall.py
+++ ApiCall.py
@@ -1,8 +1,8 @@
 import pip._vendor.requests
 from pip._vendor import requests
 
-countryCity = 'Brampton'
-api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(countryCity = 'Brampton'
+City = 'Brampton'
+api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(City = 'Brampton'
 )
 response = requests.get(api_url, headers={'X-Api-Key': '85PFMA5rymDhOSCMI6zKnw==wNtRNdqXJh5UQbNG'})
 if response.status_code == requests.codes.ok:

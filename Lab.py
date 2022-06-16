195fb02f13ac7cf7f3ac12920bb0ecc0f4097bda
Manpreet <manpreet2197@gmail.com>
2022-06-15 23:27:37 -0400

    Updated the code to perform get request

--- Lab.py
+++ Lab.py
@@ -3,15 +3,19 @@ import pip._vendor.requests
 from pip._vendor import requests
 import random
 
-api_url = 'https://ptsv2.com/t/labwork/post'
-usernumber = random.randint(0,150)
-payload = {"userId": usernumber,
-"username":'labwork',
-'userpass':'123',
-'favcolour':'yellow',
-'favnumber':41,
-}
-headers = {'content-type': 'application/json'}
+# api_url = 'https://ptsv2.com/t/labwork/post'
+# usernumber = random.randint(0,150)
+# payload = {"userId": usernumber,
+# "username":'labwork',
+# 'userpass':'123',
+# 'favcolour':'yellow',
+# 'favnumber':41,
+# }
+# headers = {'content-type': 'application/json'}
  
-r = requests.post(api_url, auth=('labwork','123'), data=payload, headers=headers)
+# r = requests.post(api_url, auth=('labwork','123'), data=payload, headers=headers)
+
+get_uri='https://ptsv2.com/t/labwork/d/latest/json'
+g = requests.get(get_uri)
+print(g.json())
 

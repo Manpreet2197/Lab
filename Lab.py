6b90d00c2b700cce16b0b9d7885e116d0e80e233
Manpreet <manpreet2197@gmail.com>
2022-06-15 23:25:28 -0400

    Created lab.py with Post code

---
+++ Lab.py
@@ -0,0 +1,17 @@
+
+import pip._vendor.requests
+from pip._vendor import requests
+import random
+
+api_url = 'https://ptsv2.com/t/labwork/post'
+usernumber = random.randint(0,150)
+payload = {"userId": usernumber,
+"username":'labwork',
+'userpass':'123',
+'favcolour':'yellow',
+'favnumber':41,
+}
+headers = {'content-type': 'application/json'}
+ 
+r = requests.post(api_url, auth=('labwork','123'), data=payload, headers=headers)
+
